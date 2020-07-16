2a7b4d4bda53585edf08e60568dc4bf58e3a5684
Tyler Long <tyler4long@gmail.com>
2020-07-16 14:13:03 -0700

    Reuse refresh token test

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 const SDK = require('ringcentral')
 const delay = require('timeout-as-promise')
 
-var rcsdk = new SDK({
+var rc = new SDK({
   server: process.env.RINGCENTRAL_SERVER_URL,
   appKey: process.env.RINGCENTRAL_CLIENT_ID,
   appSecret: process.env.RINGCENTRAL_CLIENT_SECRET
 })
 
 ;(async () => {
-  await rcsdk.platform().login({
+  await rc.platform().login({
     username: process.env.RINGCENTRAL_USERNAME,
     extension: process.env.RINGCENTRAL_EXTENSION,
     password: process.env.RINGCENTRAL_PASSWORD
   })
 
-  var subscription = rcsdk.createSubscription()
+  const oldToken = await rc.platform().auth().data();
+  console.log('old token: ', oldToken);
 
-  subscription.on(subscription.events.notification, function (msg) {
-    console.log(msg, msg.body)
-  })
+  await delay(3000);
 
-  await subscription.setEventFilters(['/restapi/v1.0/account/~/extension/~/message-store']).register()
+  // first API call
+  let r = await rc.platform().get('/restapi/v1.0/account/~/extension/~')
+  console.log('first api call', r.json().id)
 
-  while (true) {
-    await rcsdk.platform().post('/restapi/v1.0/account/~/extension/~/sms', {
-      from: { phoneNumber: process.env.RINGCENTRAL_USERNAME },
-      to: [{ phoneNumber: process.env.RINGCENTRAL_RECEIVER }],
-      text: 'hello world'
-    })
-    await delay(10000)
-  }
+  await delay(3000);
+
+  // refresh it
+  await rc.platform().refresh();
+  const newToken = await rc.platform().auth().data();
+  console.log('new token: ',  newToken);
+
+  await delay(3000);
+
+  // restore the old token
+  rc.platform().auth().setData(oldToken);
+
+  await delay(3000);
+
+  // refresh it again
+  await rc.platform().refresh();
+  const newToken2 = await rc.platform().auth().data();
+  console.log('new token 2: ',  newToken2);
+
+  await delay(3000);
+
+  // second API call
+  r = await rc.platform().get('/restapi/v1.0/account/~/extension/~')
+  console.log('second api call', r.json().id)
+
+  await rc.platform().logout();
 })()
